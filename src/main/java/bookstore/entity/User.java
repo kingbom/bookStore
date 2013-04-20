0494889f8cbea4d074c9e07905104943dc8e2a3a
Jared Stemen <jstemen@gmail.com>
2013-04-20 11:13:13 -0400

    Mostly merged in the Marks code in.  Still might be a efew things left to do.

--- src/main/java/bookstore/entity/User.java
+++ src/main/java/bookstore/entity/User.java
@@ -12,66 +12,59 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class User {
+	public enum UserStatus {
+		NEW, INVALID, VALID, LOGGED_IN
+	};
+	
 	private String addressFirstLine;
+	private boolean addressFirstLineMissing=true;
 	private String addressSecondLine;
+	private boolean addressSecondLineMissing=true;
+	
 	private String city;
-	@Column(unique = true)
-	@Email
+	private Boolean cityMissing;
+	
+	//@Column(unique = true)
+	//@Email
 	private String email;
-	@NotEmpty
+	boolean emailAddressDupl = true;
+	boolean emailAddressMissing = true;
+	//@NotEmpty
 	private String firstName;
+	boolean firstNameMissing = true;
 	@javax.persistence.Id
 	@GeneratedValue
 	private Long Id;
-	@NotEmpty
+	//@NotEmpty
 	private String lastName;
-	@NotEmpty
+	boolean lastNameMissing = true;
+	//@NotEmpty
 	private String password;
+	boolean passwordMissing = true;;
 	private String state;
+	private boolean stateMissing = true;
+	private UserStatus userStatus = UserStatus.NEW;
 	private String zipcode;
+	private boolean zipcodeMissing = true;
 	
 	//No arg constructor needed for jpa
-	public User(){};
+	public User(){}
 	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime
-				* result
-				+ ((addressFirstLine == null) ? 0 : addressFirstLine.hashCode());
-		result = prime
-				* result
-				+ ((addressSecondLine == null) ? 0 : addressSecondLine
-						.hashCode());
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result
-				+ ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result
-				+ ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result
-				+ ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((state == null) ? 0 : state.hashCode());
-		result = prime * result + ((zipcode == null) ? 0 : zipcode.hashCode());
-		return result;
+	public User(String addressFirstLine, String addressSecondLine, String city,
+			String email, String firstName, String lastName,
+			String password, String state, String zipcode) {
+		super();
+		this.addressFirstLine = addressFirstLine;
+		this.addressSecondLine = addressSecondLine;
+		this.city = city;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.state = state;
+		this.zipcode = zipcode;
 	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "User [addressFirstLine=" + addressFirstLine
-				+ ", addressSecondLine=" + addressSecondLine + ", city=" + city
-				+ ", email=" + email + ", firstName=" + firstName + ", Id="
-				+ Id + ", lastName=" + lastName + ", password=" + password
-				+ ", state=" + state + ", zipcode=" + zipcode + "]";
-	}
-
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
@@ -130,40 +123,37 @@ public class User {
 		} else if (!zipcode.equals(other.zipcode))
 			return false;
 		return true;
-	}
-
-	public User(String addressFirstLine, String addressSecondLine, String city,
-			String email, String firstName, String lastName,
-			String password, String state, String zipcode) {
-		super();
-		this.addressFirstLine = addressFirstLine;
-		this.addressSecondLine = addressSecondLine;
-		this.city = city;
-		this.email = email;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.password = password;
-		this.state = state;
-		this.zipcode = zipcode;
-	}
+	};
+	
 	/**
 	 * @return the addressFirstLine
 	 */
 	public String getAddressFirstLine() {
 		return addressFirstLine;
 	}
+	
+	public boolean isAddrStateMissing() {
+		return (this.userStatus == UserStatus.NEW) ? false : stateMissing;
+	}
+	
+	public boolean isAddrZipMissing() {
+		return (this.userStatus == UserStatus.NEW) ? false : this.zipcodeMissing;
+	}
+
 	/**
 	 * @return the addressSecondLine
 	 */
 	public String getAddressSecondLine() {
 		return addressSecondLine;
 	}
+
 	/**
 	 * @return the city
 	 */
 	public String getCity() {
 		return city;
 	}
+
 	/**
 	 * @return the email
 	 */
@@ -200,11 +190,65 @@ public class User {
 	public String getState() {
 		return state;
 	}
+	public UserStatus getUserStatus() {
+		return userStatus;
+	}
 	/**
 	 * @return the zipcode
 	 */
 	public String getZipcode() {
 		return zipcode;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((addressFirstLine == null) ? 0 : addressFirstLine.hashCode());
+		result = prime
+				* result
+				+ ((addressSecondLine == null) ? 0 : addressSecondLine
+						.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((zipcode == null) ? 0 : zipcode.hashCode());
+		return result;
+	}
+	public boolean isAddrCityMissing() {
+		return (this.userStatus == UserStatus.NEW) ? false : cityMissing;
+	}
+	public boolean isAddrFirstLineMissing() {
+		return (this.userStatus == UserStatus.NEW) ? false : addressFirstLineMissing;
+	}
+	
+	public boolean isAddrSecondLineMissing() {
+		return (this.userStatus == UserStatus.NEW) ? false : addressSecondLineMissing;
+	}
+	public boolean isEmailAddressMissing() {
+		return (userStatus == UserStatus.NEW) ? false: emailAddressMissing;
+	}
+	public boolean isFirstNameMissing() {
+		return (userStatus == UserStatus.NEW) ? false: firstNameMissing;
+	}
+	public boolean isPasswordMissing() {
+		return (userStatus == UserStatus.NEW) ? false: passwordMissing;
+	}
+	public boolean isUserLoggedIn () {
+		return (userStatus == UserStatus.LOGGED_IN) ? true : false;
+	}
+	public boolean isUserValid () {
+		return (userStatus == UserStatus.INVALID) ? false : true;
 	}
 	
 	/**
@@ -219,12 +263,14 @@ public class User {
 	public void setAddressSecondLine(String addressSecondLine) {
 		this.addressSecondLine = addressSecondLine;
 	}
+	
 	/**
 	 * @param city the city to set
 	 */
 	public void setCity(String city) {
 		this.city = city;
 	}
+	
 	/**
 	 * @param email the email to set
 	 */
@@ -236,29 +282,67 @@ public class User {
 	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+		this.firstNameMissing = (firstName == null || firstName.isEmpty()) ? true : false;
 	}
-	/**
-	 * @param lastName the lastName to set
-	 */
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+		this.lastNameMissing = (lastName == null || lastName.isEmpty()) ? true : false;
 	}
+	
 	/**
 	 * @param password the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	/**
 	 * @param state the state to set
 	 */
 	public void setState(String state) {
 		this.state = state;
 	}
+	
+	public void setUserStatus(UserStatus userStatus) {
+		this.userStatus = userStatus;
+	}
 	/**
 	 * @param zipcode the zipcode to set
 	 */
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "User [addressFirstLine=" + addressFirstLine
+				+ ", addressSecondLine=" + addressSecondLine + ", city=" + city
+				+ ", email=" + email + ", firstName=" + firstName + ", Id="
+				+ Id + ", lastName=" + lastName + ", password=" + password
+				+ ", state=" + state + ", zipcode=" + zipcode + "]";
+	}
+	
+	public boolean validateUser () {
+		
+		if (!emailAddressMissing) {
+			//Jared's not sure what the best way to do this is.
+			//emailAddressDupl = (UserDB.emailExists(emailAddress)) ? true : false;
+			emailAddressDupl =false;
+		}
+		
+		if (emailAddressMissing ||
+				emailAddressDupl ||
+				firstNameMissing ||
+				lastNameMissing) {
+			userStatus = UserStatus.INVALID;
+			return true;
+		}
+		else {
+			userStatus = UserStatus.VALID;
+			return true;
+		}
 	}
 }
--- src/main/java/bookstore/workerbeans/UserDatabaseAccessor.java
+++ src/main/java/bookstore/workerbeans/UserDatabaseAccessor.java
@@ -6,35 +6,27 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import bookstore.Address;
 import bookstore.entity.User;
 import bookstore.repository.UserRepository;
 
 @Component
-public class UserBeanImpl implements UserBean{
+public class UserDatabaseAccessor{
 
 	@Autowired
 	private UserRepository repository;
 
-	@Override
 	public User getUser(String email, String password) {
 		return repository.findUserByEmailIgnoreCaseAndPassword(email, password);
 	}
 
-	@Override
-	public User insertUser(HttpServletRequest request, HttpServletResponse response) {
-
-		User user = (User) request.getSession().getAttribute("userbean");
+	public User insertUser(User user) {
 
 		User dbUser = repository.save(user);	
 
 		return dbUser;
 	}
 
-	@Override
-	public User updateUser(HttpServletRequest request, HttpServletResponse response) {
-
-		User user = (User) request.getSession().getAttribute("userbean");
+	public User updateUser(User user) {
 
 		User dbUser = repository.save(user);	
 
